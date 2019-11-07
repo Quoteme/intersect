9702ffc0edeb7afef385fd67a6fcb8f218e2c0e4
quoteme <lucahappel99@gmx.de>
2019-11-08 00:08:29 +0100

    added more information about the functions

--- intersect.mjs
+++ intersect.mjs
@@ -13,12 +13,20 @@ const point2interval = (a,i) => i[0] <= a && a <= i[1];
 const interval2interval = (i,j) => j[0] <= i[0] && i[1] <= j[1];
 
 // also works on n dimensional objects
+// the following provides information for a
+// 3D Hyperplane (a cube) as parameter p
+//	[
+//		(min X, max X),
+//		(min Y, max Y),
+//		(min Z, max Z)
+//	]
 // point2plane :: Real a => (a,a) -> [(a,a)] -> Boolean
 const point2plane = (a,p) => a
 	.map((x,i) => point2interval(x,p[i]))
 	.every(x => x==true)
 
 // also works on n dimensional objects
+// planes are defined as in the section point2plane
 // plane2plane :: Real a => [(a,a)] -> [(a,a)] -> Boolean
 const plane2plane = (p,q) =>
 	point2plane(
