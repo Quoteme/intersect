dcf5fd6523c7d8b5093d915c400fb99c8b23e11c
quoteme <lucahappel99@gmx.de>
2019-11-08 00:03:53 +0100

    first commit

---
+++ intersect.mjs
@@ -0,0 +1,48 @@
+// point2point :: Num a => [a] -> [a] -> Boolean
+const point2point = (a,b) => a.length == b.length
+	? a
+		.map((x,i) => x==b[i])
+		.every(x => x==true)
+	: false;
+	// TODO: add condition if one point has only 0s ater r the the other one is only r long
+
+// point2interval :: Real a => a -> (a,a) -> Boolean
+const point2interval = (a,i) => i[0] <= a && a <= i[1];
+
+// interval2interval :: Real a => (a,a) -> (a,a) -> Boolean
+const interval2interval = (i,j) => j[0] <= i[0] && i[1] <= j[1];
+
+// also works on n dimensional objects
+// point2plane :: Real a => (a,a) -> [(a,a)] -> Boolean
+const point2plane = (a,p) => a
+	.map((x,i) => point2interval(x,p[i]))
+	.every(x => x==true)
+
+// also works on n dimensional objects
+// plane2plane :: Real a => [(a,a)] -> [(a,a)] -> Boolean
+const plane2plane = (p,q) =>
+	point2plane(
+		p.map(x => x[0]),
+		q
+	) &&
+	point2plane(
+		p.map(x => x[1]),
+		q
+	)
+
+// also works on n dimensional Spheres
+// point2circle :: Num a => [a] -> Real -> [a] -> ([a] -> Real) -> Boolean
+const point2circle = (p,r,q=p.map(_=>0),d=Math.hypot) =>
+	d(...p.map((x,i) => x-q[i])) <= r;
+
+// plane2circle :: Real a => [(a,a)] -> a -> [a] -> ([a] -> Real) -> Boolean
+// const plane2circle =
+
+export(
+	point2point,
+	point2interval,
+	interval2interval,
+	point2plane,
+	plane2plane,
+	point2circle
+)
