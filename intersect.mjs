e89ef6635b1000b97d5cff2350d5782734941b5a
Luca Happel <lucahappel99@gmx.de>
2020-02-14 15:04:21 +0100

    small changes

--- intersect.mjs
+++ intersect.mjs
@@ -1,5 +1,9 @@
+// @Author	: Luca Leon Happel
+// @Date	: 14.11.2019
+// @URL		: https://github.com/Quoteme/intersect
+
 // point2point :: Num a => [a] -> [a] -> Boolean
-const point2point = (a,b) => a.length == b.length
+export const point2point = (a,b) => a.length == b.length
 	? a
 		.map((x,i) => x==b[i])
 		.every(x => x==true)
@@ -7,10 +11,10 @@ const point2point = (a,b) => a.length == b.length
 	// TODO: add condition if one point has only 0s ater r the the other one is only r long
 
 // point2interval :: Real a => a -> (a,a) -> Boolean
-const point2interval = (a,i) => i[0] <= a && a <= i[1];
+export const point2interval = (a,i) => i[0] <= a && a <= i[1];
 
 // interval2interval :: Real a => (a,a) -> (a,a) -> Boolean
-const interval2interval = (i,j) => j[0] <= i[0] && i[1] <= j[1];
+export const interval2interval = (i,j) => j[0] <= i[0] && i[1] <= j[1];
 
 // also works on n dimensional objects
 // the following provides information for a
@@ -21,7 +25,7 @@ const interval2interval = (i,j) => j[0] <= i[0] && i[1] <= j[1];
 //		(min Z, max Z)
 //	]
 // point2plane :: Real a => (a,a) -> [(a,a)] -> Boolean
-const point2plane = (a,p) =>
+export const point2plane = (a,p) =>
 	a.length == p.length
 	? a
 		.map((x,i) => point2interval(x,p[i]))
@@ -31,7 +35,7 @@ const point2plane = (a,p) =>
 // also works on n dimensional objects
 // planes are defined as in the section point2plane
 // plane2plane :: Real a => [(a,a)] -> [(a,a)] -> Boolean
-const plane2plane = (p,q) =>
+export const plane2plane = (p,q) =>
 	point2plane(
 		p.map(x => x[0]),
 		q
@@ -43,15 +47,15 @@ const plane2plane = (p,q) =>
 
 // also works on n dimensional Spheres
 // point2circle :: Num a => [a] -> Real -> [a] -> ([a] -> Real) -> Boolean
-const point2circle = (p,r,q=p.map(_=>0),d=Math.hypot) =>
+export const point2circle = (p,r,q=p.map(_=>0),d=Math.hypot) =>
 	d(...p.map((x,i) => x-q[i])) <= r;
 
 // plane2circle :: Real a => [(a,a)] -> a -> [a] -> ([a] -> Real) -> Boolean
-// const plane2circle =
+// const plane2circle = (p,r,c,d=Math.hypot) =>
 
 // determines if a point is an element of a n-D array
 // point2Array :: Int a => [a] -> [a]^n -> Boolean
-const point2Array = (a,arr) =>
+export const point2Array = (a,arr) =>
 	a.filter(x => !Number.isInteger(x)) == 0
 	? new Array(a.length)
 		.fill(0)
@@ -59,13 +63,3 @@ const point2Array = (a,arr) =>
 		.map((e,i) => point2interval(a[i],e))
 		.every(x => x==true)
 	: undefined
-
-export {
-	point2point,
-	point2interval,
-	interval2interval,
-	point2plane,
-	plane2plane,
-	point2circle,
-	point2Array
-}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# intersect.js
+
+A library to solve all your collision and intersection problems
+(no matter what dimension you work in).
+
+Documentation is provided as comments in the source code.
